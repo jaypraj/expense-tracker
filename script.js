5edecad81196dfda99360deb1654f0e705b0d0d5
Jaymin Prajapati <jaymincprajapati@gmail.com>
2026-02-04 21:38:11 +0530

    Improve delete functionality by adding ID to the expense object and the table row

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 class Expense {
+    id;
     amount;
     category;
     note;
     dateTime;
 
     constructor(amount, category, note, dateTime) {
+        this.id = crypto.randomUUID();
         this.amount = amount;
         this.category = category;
         this.note = note;
@@ -19,7 +21,13 @@ let dateTime = document.querySelector("#dateTime");
 let addBtn = document.querySelector("#addBtn");
 let expensesTable = document.querySelector(".expenses");
 
-const expenses = [];
+expensesTable.addEventListener("click", (e) => {
+    if (e.target.dataset.action == "delete") {
+        deleteExpense(e.target.parentElement.parentElement.dataset.expenseId);
+    }
+});
+
+let expenses = [];
 
 let form = document.querySelector("#expenseForm");
 
@@ -28,42 +36,57 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 });
 
-addBtn.addEventListener("click", () => {
+addBtn.addEventListener("click", () => addExpense());
+
+function addExpense() {
     const expense = new Expense(amount.value, category.value, note.value, dateTime.value);
-
-    let row = document.createElement("tr");
-
-    let amountCol = document.createElement("td");
-    let categoryCol = document.createElement("td");
-    let noteCol = document.createElement("td");
-    let dateCol = document.createElement("td");
-    let deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "x";
-
-    
-    amountCol.textContent = expense.amount;
-    categoryCol.textContent = expense.category;
-    noteCol.textContent = expense.note;
-    dateCol.textContent = expense.dateTime;
-    
-    row.appendChild(amountCol);
-    row.appendChild(categoryCol)
-    row.appendChild(noteCol);
-    row.appendChild(dateCol);
-    row.appendChild(document.createElement("td").appendChild(deleteBtn));
-    
-    expensesTable.style.display = "table";
-    document.querySelector(".expenses tbody").appendChild(row);
-    form.reset();
-    
-    deleteBtn.addEventListener("click", (e) => deleteExpense(e));
     expenses.push(expense);
-});
 
-function deleteExpense(e) {
-    e.target.parentElement.remove();
+    renderExpenses();
 
-    if (!document.querySelector(".expenses tbody").hasChildNodes()) {
+    form.reset();
+}
+
+function deleteExpense(expenseId) {
+    console.log(expenses);
+    expenses = expenses.filter((expense) => expense.id != expenseId);
+    console.log(expenses);
+
+    renderExpenses();
+
+    if (expenses.length == 0) {
         expensesTable.style.display = "None";
     }
+}
+
+function renderExpenses() {
+    document.querySelector(".expenses tbody").innerHTML = "";
+    expenses.forEach((expense) => {
+        let row = document.createElement("tr");
+
+        let amountCol = document.createElement("td");
+        let categoryCol = document.createElement("td");
+        let noteCol = document.createElement("td");
+        let dateCol = document.createElement("td");
+        let deleteBtnCol = document.createElement("td");
+        let deleteBtn = document.createElement("button");
+        deleteBtn.dataset.action = "delete";
+        deleteBtn.textContent = "x";
+
+        row.dataset.expenseId = expense.id;
+        amountCol.textContent = expense.amount;
+        categoryCol.textContent = expense.category;
+        noteCol.textContent = expense.note;
+        dateCol.textContent = expense.dateTime;
+
+        row.appendChild(amountCol);
+        row.appendChild(categoryCol);
+        row.appendChild(noteCol);
+        row.appendChild(dateCol);
+        deleteBtnCol.appendChild(deleteBtn);
+        row.appendChild(deleteBtnCol);
+
+        expensesTable.style.display = "table";
+        document.querySelector(".expenses tbody").appendChild(row);
+    });
 }
